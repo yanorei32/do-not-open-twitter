52e308092d26b8be448a768e3206a30c3e9e7b39
hae <33520107+hae902@users.noreply.github.com>
2020-04-19 08:11:19 +0900

    add Tweetdeck

--- do-not-open-twitter.user.js
+++ do-not-open-twitter.user.js
@@ -3,6 +3,7 @@
 // @description	Redirect to SoundCloud
 // @version		0.0.1
 // @include		https://twitter.com/*
+// @include		https://tweetdeck.twitter.com/*
 // @author		yanorei32
 // @website		https://github.com/Yanorei32/do-not-open-twitter
 // @namespace	http://yano.teamfruit.net/~rei/
