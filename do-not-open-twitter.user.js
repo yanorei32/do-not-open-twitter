ca052d0ea34f178ccb9a6db47b647ee728f18d92
hae <33520107+hae902@users.noreply.github.com>
2020-04-20 20:30:25 +0900

    I changed the URL can toggle this script.
    I changed the URL from google.com to soundcloud.com.

--- do-not-open-twitter.user.js
+++ do-not-open-twitter.user.js
@@ -5,7 +5,7 @@
 // @include		https://twitter.com/*
 // @include		https://mobile.twitter.com/*
 // @include		https://tweetdeck.twitter.com/*
-// @include		https://www.google.com/*
+// @include		https://soundcloud.com/*
 // @author		yanorei32
 // @website		https://github.com/Yanorei32/do-not-open-twitter
 // @namespace	http://yano.teamfruit.net/~rei/
@@ -17,7 +17,7 @@
 
 (function() {
 	'use strict';
-	if (/google.com/.test(location.href)){
+	if (/soundcloud.com/.test(location.href)){
 		return;
 	}
 	location.href = 'https://soundcloud.com/you/likes';
