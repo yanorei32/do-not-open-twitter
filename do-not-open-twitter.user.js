ef0e644b17338c37d78be5c3003e78aea1bd8332
hae <33520107+hae902@users.noreply.github.com>
2020-04-20 15:20:52 +0900

    I made easy to switch on/off of this script.
    You can use it when access to google currently.
    (Implementation is quite crude! sorry!)

--- do-not-open-twitter.user.js
+++ do-not-open-twitter.user.js
@@ -5,6 +5,7 @@
 // @include		https://twitter.com/*
 // @include		https://mobile.twitter.com/*
 // @include		https://tweetdeck.twitter.com/*
+// @include		https://www.google.com/*
 // @author		yanorei32
 // @website		https://github.com/Yanorei32/do-not-open-twitter
 // @namespace	http://yano.teamfruit.net/~rei/
@@ -16,5 +17,8 @@
 
 (function() {
 	'use strict';
+	if (/google.com/.test(location.href)){
+		return;
+	}
 	location.href = 'https://soundcloud.com/you/likes';
 })();
