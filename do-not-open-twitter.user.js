4aa95066c699f671f20cb5684b2aeea190db9446
Ray Yano | あねてあ <yanorei@hotmail.co.jp>
2024-06-20 15:44:33 +0900

    Update do-not-open-twitter.user.js

--- do-not-open-twitter.user.js
+++ do-not-open-twitter.user.js
@@ -1,15 +1,16 @@
 // ==UserScript==
 // @name		DO NOT OPEN TWITTER
 // @description	Redirect to SoundCloud
-// @version		0.3.1
-// @include		https://twitter.com/*
-// @include		https://mobile.twitter.com/*
-// @include		https://tweetdeck.twitter.com/*
-// @include		https://soundcloud.com/*
+// @version		0.4.0
+// @match		https://x.com/*
+// @match		https://twitter.com/*
+// @match		https://mobile.twitter.com/*
+// @match		https://tweetdeck.twitter.com/*
+// @match		https://soundcloud.com/*
 // @author		yanorei32
 // @website		https://github.com/yanorei32/do-not-open-twitter
 // @namespace	https://yr32.net/
-// @updateURL	https://github.com/yanorei32/do-not-open-twitter/do-not-open-twitter.user.js
+// @updateURL	https://github.com/yanorei32/do-not-open-twitter/raw/master/do-not-open-twitter.user.js
 // @license		The Unlicense
 // @grant		none
 // @run-at		document-start
